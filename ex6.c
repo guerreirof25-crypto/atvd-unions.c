ff8c6200bd9a3d5854c6308a1375e80345d400ea
guerreirof25-crypto <guerreirof25@gmail.com>
2025-09-24 08:36:24 -0300

    Create ex6.c

---
+++ ex6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+union ponto{
+    struct{
+        int x;
+        int y;
+        int z;
+    };
+    int vec[3];
+};
+
+int main(){
+    union ponto pt;
+    pt.vec[0]=10;
+    pt.vec[1]=20;
+    pt.vec[2]=30;
+    printf("(%d,%d,%d)\n", pt.x, pt.y, pt.z);
+    return 0;
+}
